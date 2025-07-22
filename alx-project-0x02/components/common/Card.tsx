ad75801082cde004ee9320b621d66acd472cd78e
Mokete <ematyabekana@gmail.com>
2025-07-22 20:47:02 +0200

    Updated the import statement of the CardProps.

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,10 +1,10 @@
-import {type CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card:React.FC<CardProps>=({title,content})=>{
     return(
         <div className="p-4 shadow-sm rounded-sm">
           <h1 className="text-2xl font-bold mb-2">{title}</h1>
-          <p>{content}</p>
+          <p className="text-gray-500">{content}</p>
         </div>
     )
 }
